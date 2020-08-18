7a953b5deae1d3f37ad33884b46564c5fe1d22dc
ffreemt <yucongo+fmt@gmail.com>
2020-08-18 11:41:52 +0800

    bump version 0.0.3

--- xiaoniu_tr/__init__.py
+++ xiaoniu_tr/__init__.py
@@ -1,5 +1,7 @@
 from xiaoniu_tr.xiaoniu_tr import xiaoniu_tr  # type: ignore
 
-__version__ = '0.0.2'
-__date__ = '2019.8.15'
-VERSION = __version__
+# version__ = '0.0.2'
+# date__ = '2019.8.15'
+__version__ = '0.0.3'
+__date__ = '2020.8.17'
+VERSION = __version__.split(".")
